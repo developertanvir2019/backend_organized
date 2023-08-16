f8b94fd6f15bdd663736fe96318558e8201c37f3
developertanvir2019 <developertanvir2019@gmail.com>
2023-08-17 00:00:06 +0600

    complete single user find

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express"
-import { createUserToDB, getUsersFromDB } from "./user.service"
+import { createUserToDB, getUserByIdFromDB, getUsersFromDB } from "./user.service"
 
 
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -21,4 +21,11 @@ export const getUsers = async (req: Request, res: Response, next: NextFunction) 
     })
 }
 
-// route -> controller-> service
+export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const user = await getUserByIdFromDB(id)
+    res.status(200).json({
+        status: 'success',
+        data: user
+    })
+}
